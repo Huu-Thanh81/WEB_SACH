d71a7972589d64f97f3ca246e3d39b7dcb41355d
Bùi Hữu Thành <buithanh812004@gmail.com>
2024-10-03 09:55:56 +0700

    update version 1

---
+++ assets/javascript.js
@@ -0,0 +1,12 @@
+document.getElementById('submitForm').addEventListener('click', function () {
+    var form = document.getElementById('contactForm');
+    if (form.checkValidity()) {
+      $('#exampleModal').modal('show'); // Mở modal nếu form hợp lệ
+      // Tự động đóng modal xác nhận sau 3 giây
+      setTimeout(function () {
+      $('#exampleModal').modal('hide');
+      }, 3000);
+    } else {
+      form.reportValidity(); // Kiểm tra và báo lỗi nếu form không hợp lệ
+    }
+});
